227ef03dadd0204ec8869b9da26c3e7f8dd75a3a
Kevin Alexis Contreras <36680385+KiritoDv@users.noreply.github.com>
2022-12-01 20:11:22 -0600

    Fixed responsive on finish screen

--- lib/features/create_finish/create_finish_screen.dart
+++ lib/features/create_finish/create_finish_screen.dart
@@ -40,71 +40,80 @@ class _CreateFinishBottomBarModalState
               content: Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(20),
-                  child: ListView(children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Contents:", style: textTheme.subtitle1!.copyWith(decoration: TextDecoration.underline)),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8, right: 20),
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            physics: const ClampingScrollPhysics(),
-                            itemCount: viewModel.entries.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              String key = viewModel.entries.keys.elementAt(index);
-                              return Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text(key, style: textTheme.subtitle2),
-                                  for (var file in viewModel.entries[key]!.iterables)
-                                    Row(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: ListView(children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Contents:", style: textTheme.subtitle1!.copyWith(decoration: TextDecoration.underline)),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 8, right: 20),
+                                child: ListView.builder(
+                                  shrinkWrap: true,
+                                  physics: const ClampingScrollPhysics(),
+                                  itemCount: viewModel.entries.length,
+                                  itemBuilder: (BuildContext context, int index) {
+                                    String key = viewModel.entries.keys.elementAt(index);
+                                    return Column(
                                       crossAxisAlignment: CrossAxisAlignment.start,
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      mainAxisSize: MainAxisSize.min,
+                                      mainAxisAlignment: MainAxisAlignment.start,
                                       children: [
-                                        Expanded(child: Text("\u2022 ${file.path}", overflow: TextOverflow.ellipsis, style: textTheme.bodyText2)),
-                                        SizedBox(
-                                          width: 20,
-                                          height: 20,
-                                          child: IconButton(
-                                            iconSize: 20,
-                                            splashRadius: 20,
-                                            padding: EdgeInsets.zero,
-                                            icon: const Icon(Icons.delete),
-                                            onPressed: () {
-                                              viewModel.onRemoveFile(file, key);
-                                            },
-                                          )),
-                                      ],
-                                    ),
-                              ]);
-                            }))
-                      ]),
-                      const Spacer(),
-                      ElevatedButton(
-                        onPressed: viewModel.entries.isNotEmpty ? () {
-                          viewModel.onGenerateOTR(() {
-                            widget.dismissCallback();
-                          });
-                        } : null,
-                        style: ElevatedButton.styleFrom(backgroundColor: Colors.green, minimumSize: Size(
-                          MediaQuery.of(context).size.width * 0.5, 50)
-                        ),
-                        child: viewModel.isGenerating
-                          ? Container(
-                              width: 24,
-                              height: 24,
-                              padding: const EdgeInsets.all(2.0),
-                              child: const CircularProgressIndicator(
-                                color: Colors.white,
-                                strokeWidth: 3,
-                              ),
-                            )
-                          : const Text('Generate OTR')
+                                        Text(key, style: textTheme.subtitle2),
+                                        for (var file in viewModel.entries[key]!.iterables)
+                                          Row(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: [
+                                              Expanded(child: Text("\u2022 ${file.path}", overflow: TextOverflow.ellipsis, style: textTheme.bodyText2)),
+                                              SizedBox(
+                                                width: 20,
+                                                height: 20,
+                                                child: IconButton(
+                                                  iconSize: 20,
+                                                  splashRadius: 20,
+                                                  padding: EdgeInsets.zero,
+                                                  icon: const Icon(Icons.delete),
+                                                  onPressed: () {
+                                                    viewModel.onRemoveFile(file, key);
+                                                  },
+                                                )),
+                                            ],
+                                          ),
+                                    ]);
+                                  }))
+                            ]),
+
+                        ]),
                       ),
-                  ]),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20.0),
+                        child: ElevatedButton(
+                          onPressed: viewModel.entries.isNotEmpty ? () {
+                            viewModel.onGenerateOTR(() {
+                              widget.dismissCallback();
+                            });
+                          } : null,
+                          style: ElevatedButton.styleFrom(backgroundColor: Colors.green, minimumSize: Size(
+                            MediaQuery.of(context).size.width * 0.5, 50)
+                          ),
+                          child: viewModel.isGenerating
+                            ? Container(
+                                width: 24,
+                                height: 24,
+                                padding: const EdgeInsets.all(2.0),
+                                child: const CircularProgressIndicator(
+                                  color: Colors.white,
+                                  strokeWidth: 3,
+                                ),
+                              )
+                            : const Text('Generate OTR')
+                        ),
+                      ),
+                    ],
+                  ),
                 )),
             ),
         ),
