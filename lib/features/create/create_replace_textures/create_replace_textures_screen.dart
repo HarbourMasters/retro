877ca52178019f4e1e332f15bd4d72c4869e0588
PurpleHato <linkvssangoku.jr@gmail.com>
2022-12-08 19:51:48 +0100

    TWEAK: fforgot one word on two keys
    Co-Authored-By: David Chavez <david@dcvz.io>

--- lib/features/create/create_replace_textures/create_replace_textures_screen.dart
+++ lib/features/create/create_replace_textures/create_replace_textures_screen.dart
@@ -26,8 +26,8 @@ class _CreateReplaceTexturesScreenState extends State<CreateReplaceTexturesScree
     final AppLocalizations i18n = AppLocalizations.of(context)!;
 
     return CustomScaffold(
-      title: i18n.createReaplceTexturesOption,
-      subtitle: i18n.createReaplceTexturesOptionDescription,
+      title: i18n.createReaplceTexturesScreen_Option,
+      subtitle: i18n.createReaplceTexturesScreen_OptionDescription,
       onBackButtonPressed: () {
         viewModel.reset();
         Navigator.pop(context);
