6f50a585659f6c990d7efe008079ac349aacd937
David Chavez <david@dcvz.io>
2023-02-02 00:42:56 +0100

    Feature: Async Processing (#34)

--- lib/features/create/create_custom_sequences/create_custom_sequences_viewmodel.dart
+++ lib/features/create/create_custom_sequences/create_custom_sequences_viewmodel.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:retro/utils/log.dart';
 import 'package:tuple/tuple.dart';
 import 'package:path/path.dart' as p;
+
+typedef SequenceMetaPair = Tuple2<File, File>; 
 
 class CreateCustomSequencesViewModel extends ChangeNotifier {
   String? selectedFolderPath;
@@ -22,35 +24,31 @@ class CreateCustomSequencesViewModel extends ChangeNotifier {
 
     if (selectedDirectory != null) {
       selectedFolderPath = selectedDirectory;
+      isProcessing = true;
       notifyListeners();
-      listSequenceMetaPairs();
+
+      sequenceMetaPairs = await compute(listSequenceMetaPairs, selectedFolderPath!);
+      isProcessing = false;
+      notifyListeners();
     }
   }
+}
 
-  listSequenceMetaPairs() {
-    if (selectedFolderPath == null) {
-      return;
+List<SequenceMetaPair> listSequenceMetaPairs(String folderPath) {
+  List<FileSystemEntity> files = Directory(folderPath).listSync(recursive: true);
+  List<FileSystemEntity> sequenceFiles = files.where((file) => file.path.endsWith('.seq')).toList();
+
+  List<Tuple2<File, File>> sequenceMetaPairs = [];
+  for (FileSystemEntity sequenceFile in sequenceFiles) {
+    String sequenceFileName = sequenceFile.path.split(Platform.pathSeparator).last;
+    String sequenceFileNameWithoutExtension = p.basenameWithoutExtension(sequenceFileName);
+    File metaFile = File(p.join(sequenceFile.parent.path, '$sequenceFileNameWithoutExtension.meta'));
+    if(!metaFile.existsSync()) {
+      log('Meta file not found for sequence file: $sequenceFileName! Skipping.', level: LogLevel.error);
+      continue;
     }
-
-    isProcessing = true;
-    notifyListeners();
-    List<FileSystemEntity> files = Directory(selectedFolderPath!).listSync(recursive: true);
-    List<FileSystemEntity> sequenceFiles = files.where((file) => file.path.endsWith('.seq')).toList();
-
-    List<Tuple2<File, File>> sequenceMetaPairs = [];
-    for (FileSystemEntity sequenceFile in sequenceFiles) {
-      String sequenceFileName = sequenceFile.path.split(Platform.pathSeparator).last;
-      String sequenceFileNameWithoutExtension = p.basenameWithoutExtension(sequenceFileName);
-      File metaFile = File(p.join(sequenceFile.parent.path, '$sequenceFileNameWithoutExtension.meta'));
-      if(!metaFile.existsSync()) {
-        log('Meta file not found for sequence file: $sequenceFileName! Skipping.', level: LogLevel.error);
-        continue;
-      }
-      sequenceMetaPairs.add(Tuple2(sequenceFile as File, metaFile));
-    }
-
-    this.sequenceMetaPairs = sequenceMetaPairs;
-    isProcessing = false;
-    notifyListeners();
+    sequenceMetaPairs.add(Tuple2(sequenceFile as File, metaFile));
   }
+
+  return sequenceMetaPairs;
 }
