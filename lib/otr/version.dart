7c987e226b68e52dea16af4e8e074db0d55245a1
David Chavez <david@dcvz.io>
2023-03-02 00:22:51 +0100

    Use spock as right version for asset headers

--- lib/otr/version.dart
+++ lib/otr/version.dart
@@ -7,12 +7,17 @@ enum Version {
   zhora(3),
   // 1982: Tron
   flynn(4),
-  bradley(5);
+  bradley(5),
+  gibbs(6),
+  // 1982: Star Trek WoK
+  khan(7),
+  spock(8);
 
   const Version(this.value);
   final int value;
 
   static Version fromValue(int value) {
-    return Version.values.firstWhere((Version version) => version.value == value);
+    return Version.values
+        .firstWhere((Version version) => version.value == value);
   }
 }
