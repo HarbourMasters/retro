607d73204dd5ef2f5bde9a9c9b8313fcf1b639cb
David Chavez <david@dcvz.io>
2023-05-12 00:14:35 +0200

    Add small fixes

--- lib/features/create/create_custom/create_custom_screen.dart
+++ lib/features/create/create_custom/create_custom_screen.dart
@@ -26,67 +26,72 @@ class _CreateCustomScreenState extends State<CreateCustomScreen> {
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     final TextTheme textTheme = theme.textTheme;
-    final CreateCustomViewModel viewModel = Provider.of<CreateCustomViewModel>(context);
-    final CreateFinishViewModel finishViewModel = Provider.of<CreateFinishViewModel>(context);
+    final CreateCustomViewModel viewModel =
+        Provider.of<CreateCustomViewModel>(context);
+    final CreateFinishViewModel finishViewModel =
+        Provider.of<CreateFinishViewModel>(context);
     final AppLocalizations i18n = AppLocalizations.of(context)!;
 
     return CustomScaffold(
-      title: i18n.createCustomScreen_title,
-      subtitle: i18n.createCustomScreen_subtitle,
-      onBackButtonPressed: () {
-        viewModel.onDiscardFiles();
-        Navigator.of(context).pop();
-      },
-      content: Expanded(
-        child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0),
-                  child: OutlinedButton(
-                    onPressed: viewModel.onSelectFiles,
-                    style: ElevatedButton.styleFrom(minimumSize: const Size(200, 50)),
-                    child: Text(i18n.createCustomScreen_selectButton)
-                  ),
-                ),
-                Text(viewModel.path),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 4.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  '${i18n.createCustomScreen_fileToInsert}${viewModel.files.length}',
-                  style: textTheme.subtitle1,
-                ),
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: viewModel.files.length,
-                itemBuilder: (context, index) {
-                  return Text(p.relative(viewModel.files[index].path, from: viewModel.path));
-                }
-              )
-            ),
-            ElevatedButton(
-              onPressed: viewModel.files.isNotEmpty && viewModel.path.isNotEmpty ? () {
-                finishViewModel.onAddCustomStageEntries(viewModel.files, viewModel.path);
-                viewModel.onDiscardFiles();
-                Navigator.of(context).popUntil(ModalRoute.withName("/create_selection"));
-              } : null,
-              style: ElevatedButton.styleFrom(minimumSize: Size(
-                MediaQuery.of(context).size.width * 0.5, 50)
-              ),
-              child: Text(i18n.createCustomScreen_stageFiles)
-            ),
-          ],
-        )
-      )
-    ));
+        title: i18n.createCustomScreen_title,
+        subtitle: i18n.createCustomScreen_subtitle,
+        onBackButtonPressed: () {
+          viewModel.reset();
+          Navigator.of(context).pop();
+        },
+        content: Expanded(
+            child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 12.0),
+                          child: OutlinedButton(
+                              onPressed: viewModel.onSelectFiles,
+                              style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(200, 50)),
+                              child:
+                                  Text(i18n.createCustomScreen_selectButton)),
+                        ),
+                        Text(viewModel.path),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 4.0),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          '${i18n.createCustomScreen_fileToInsert}${viewModel.files.length}',
+                          style: textTheme.subtitle1,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                        child: ListView.builder(
+                            itemCount: viewModel.files.length,
+                            itemBuilder: (context, index) {
+                              return Text(p.relative(
+                                  viewModel.files[index].path,
+                                  from: viewModel.path));
+                            })),
+                    ElevatedButton(
+                        onPressed: viewModel.files.isNotEmpty &&
+                                viewModel.path.isNotEmpty
+                            ? () {
+                                finishViewModel.onAddCustomStageEntries(
+                                    viewModel.files, viewModel.path);
+                                viewModel.reset();
+                                Navigator.of(context).popUntil(
+                                    ModalRoute.withName("/create_selection"));
+                              }
+                            : null,
+                        style: ElevatedButton.styleFrom(
+                            minimumSize: Size(
+                                MediaQuery.of(context).size.width * 0.5, 50)),
+                        child: Text(i18n.createCustomScreen_stageFiles)),
+                  ],
+                ))));
   }
 }
--- lib/features/create/create_custom/create_custom_viewmodel.dart
+++ lib/features/create/create_custom/create_custom_viewmodel.dart
@@ -2,20 +2,18 @@ import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart' as p;
-import 'package:tuple/tuple.dart';
 
 class CreateCustomViewModel extends ChangeNotifier {
   List<File> files = [];
   String path = "";
 
-  onSelectedFiles(List<File> files) {
-    this.files = files;
-    notifyListeners();
+  reset() {
+    files = [];
+    path = "";
   }
 
-  onDiscardFiles() {
-    files = [];
+  onSelectedFiles(List<File> files) {
+    this.files = files;
     notifyListeners();
   }
 
@@ -25,11 +23,13 @@ class CreateCustomViewModel extends ChangeNotifier {
   }
 
   onSelectFiles() async {
-    String? selectedBaseDirectoryPath = await FilePicker.platform.getDirectoryPath();
+    String? selectedBaseDirectoryPath =
+        await FilePicker.platform.getDirectoryPath();
     onSelectedDirectory(selectedBaseDirectoryPath ?? "");
     if (selectedBaseDirectoryPath != null) {
       var directory = Directory(selectedBaseDirectoryPath);
-      var dirList = directory.list(recursive: true).where((fsEntry) => fsEntry is File);
+      var dirList =
+          directory.list(recursive: true).where((fsEntry) => fsEntry is File);
       List<File> selectedFileList = [];
       await for (final FileSystemEntity fsEntry in dirList) {
         if (fsEntry is File) {
